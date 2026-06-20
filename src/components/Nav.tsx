9cbc503fc0ff163cf7f64c33dbcc9ec97949dfcf
chnu-kim <chanuuuu@naver.com>
2026-06-20 17:26:43 +0900

    design: nav에서 치지직 링크 제거

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -66,9 +66,7 @@ export default function Nav({ initialUser }: NavProps) {
           </a>
         )}
 
-        <a href="https://chzzk.naver.com/d6e680f5b17eba0b078f978dd722c0f3" target="_blank" rel="noreferrer" className={styles.link}>치지직</a>
-
-        <a
+        <
           href="https://chzzk.naver.com/live/d6e680f5b17eba0b078f978dd722c0f3"
           target="_blank"
           rel="noreferrer"
